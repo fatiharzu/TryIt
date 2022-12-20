1c32c81ef74a19db7646a95a0d15acdcb94bbbcf
Fatih Arzu <fatiharzu1@gmail.com>
2022-12-20 10:18:57 +0100

    Initial Commit

---
+++ TryIt/TryItViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TryIt
+//
+//  Created by imac on 20.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
